dac8e210e2615cee450056a937779a416468174d
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2019-06-21 05:05:20 -0700

    mod: dynamically set config index page based on environment

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = "{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['SERVER_NAME']}/
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = isset($_SERVER['CI_ENV']) ? '' : 'index.php';
 
 /*
 |--------------------------------------------------------------------------
