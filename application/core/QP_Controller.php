4fa85e437ed97ebc32e5483b3103629649cb4c22
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2019-08-05 20:20:51 +0100

    add: terminal function to qp_controller

--- application/core/QP_Controller.php
+++ application/core/QP_Controller.php
@@ -2,23 +2,60 @@
 
 class QP_Controller extends Ci_Controller
 {
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->helper('url');
-	}
-
-	protected function load_asset($name, $ext)
+  public function __construct()
   {
-		$path = base_url("assets/{$ext}/{$name}.{$ext}");
+    parent::__construct();
+    $this->load->helper('url');
+  }
 
-		switch ($ext)
-		{
-			case 'css':
-				return "<link href='{$path}' rel='stylesheet' type='text/css'>";
+  protected function load_asset($name, $ext)
+  {
+    $path = base_url("assets/{$ext}/{$name}.{$ext}");
 
-			case 'js':
-				return "<script src='{$path}'></script>";
-		}
+    switch ($ext)
+    {
+      case 'css':
+        return "<link href='{$path}' rel='stylesheet' type='text/css'>";
+
+      case 'js':
+        return "<script src='{$path}'></script>";
+    }
+  }
+
+  function terminal($command)
+  {
+    if(function_exists('system'))
+    {
+      ob_start();
+      system($command, $return_var);
+      $output = ob_get_contents();
+      ob_end_clean();
+    }
+    else if(function_exists('passthru'))
+    {
+      ob_start();
+      passthru($command, $return_var);
+      $output = ob_get_contents();
+      ob_end_clean();
+    }
+    else if(function_exists('exec'))
+    {
+      exec($command, $output, $return_var);
+      $output = implode('n', $output);
+    }
+    else if(function_exists('shell_exec'))
+    {
+      $output = shell_exec($command) ;
+    }
+    else
+    {
+      $output = 'Command execution not possible on this system';
+      $return_var = 1;
+    }
+
+    return [
+      'output' => $output,
+      'status' => $return_var
+    ];
   }
 }
