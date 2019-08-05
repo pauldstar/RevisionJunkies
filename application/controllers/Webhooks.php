0d6456bc7cbe44f9af0a962c483b43e3e242d788
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2019-08-05 12:53:08 +0100

    Update Webhooks.php

--- application/controllers/Webhooks.php
+++ application/controllers/Webhooks.php
@@ -2,6 +2,21 @@
 
 class Webhooks extends QP_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+
+		$signature = 'sha1='.hash_hmac(
+			'sha1',
+			$_POST['payload'],
+			$_ENV['github_webhook_secret']
+		);
+
+		$valid_signature = hash_equals($signature, $_SERVER['x-hub-signature']);
+
+		if (!$valid_signature) throw new Exception('Incorrect Signature');
+	}
+
 	public function deploy()
 	{
 		// secret QDjzkg574GjPw29B
@@ -11,5 +26,8 @@ class Webhooks extends QP_Controller
 		echo '<pre>';
 		var_dump($_SERVER);
 		echo '</pre>';
+
+		$signature = 'sha1='.
+			hash_hmac('sha1', Request::getContent(), $_ENV['github_webhook_secret']);
 	}
 }
