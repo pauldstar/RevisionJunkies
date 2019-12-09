5008107dbb18f3d4cbcf27efbfb6aac0e9e568b0
Paul Ogbeiwi <paul.ogbeiwi@fifteengroup.co.uk>
2019-12-09 12:47:03 +0000

    mod: renamed/moved QP_common to an app helper file common.php, and autoloaded to 'extend' system/core/Common.php

--- application/helpers/common.php
+++ application/helpers/common.php
@@ -1,49 +1,42 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * Common Functions
+ * QP Common Functions
  *
- * Loads the base classes and executes the request.
- *
- * @package		CodeIgniter
- * @subpackage	CodeIgniter
  * @category	Common Functions
  * @author		QuePenny
- * @link		https://codeigniter.com/user_guide/
  */
 
 // ------------------------------------------------------------------------
 
-if ( ! function_exists('log_message'))
+/**
+ * Error Logging Interface
+ * Overrides log_message() in system/core/Commons.php
+ *
+ * We use this as a simple mechanism to access the logging
+ * class and send messages to be logged.
+ *
+ * @param	string	the error level: 'error', 'debug' or 'info'
+ * @param	string	the error message
+ * @return	void
+ */
+function log_message($level, $message)
 {
-	/**
-	 * Error Logging Interface
-	 *
-	 * We use this as a simple mechanism to access the logging
-	 * class and send messages to be logged.
-	 *
-	 * @param	string	the error level: 'error', 'debug' or 'info'
-	 * @param	string	the error message
-	 * @return	void
-	 */
-	function log_message($level, $message)
-	{
 		if (ENVIRONMENT === 'production' && $level === 'error')
 		{
-			error_log($message);
-			return;
+				error_log($message);
+				return;
 		}
 
 		static $_log;
 
 		if ($_log === NULL)
 		{
-			// references cannot be directly assigned to static variables, so we use an array
-			$_log[0] =& load_class('Log', 'core');
+				// references cannot be directly assigned to static variables, so we use an array
+				$_log[0] =& load_class('Log', 'core');
 		}
 
 		$_log[0]->write_log($level, $message);
-	}
 }
 
 // ------------------------------------------------------------------------
