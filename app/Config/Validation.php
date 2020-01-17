d3a629100ea029fe34e828835d07542cfc83b016
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2020-01-17 00:58:59 +0000

    Update Validation.php
    code autoindent

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -2,48 +2,48 @@
 
 class Validation
 {
-	//--------------------------------------------------------------------
-	// Setup
-	//--------------------------------------------------------------------
+  //--------------------------------------------------------------------
+  // Setup
+  //--------------------------------------------------------------------
 
-	/**
-	 * Stores the classes that contain the
-	 * rules that are available.
-	 *
-	 * @var array
-	 */
-	public $ruleSets = [
-		\CodeIgniter\Validation\Rules::class,
-		\App\Libraries\FormatRules::class,
-		\CodeIgniter\Validation\FileRules::class,
-		\CodeIgniter\Validation\CreditCardRules::class,
-	];
+  /**
+   * Stores the classes that contain the
+   * rules that are available.
+   *
+   * @var array
+   */
+  public $ruleSets = [
+    \CodeIgniter\Validation\Rules::class,
+    \App\Libraries\FormatRules::class,
+    \CodeIgniter\Validation\FileRules::class,
+    \CodeIgniter\Validation\CreditCardRules::class,
+  ];
 
-	/**
-	 * Specifies the views that are used to display the
-	 * errors.
-	 *
-	 * @var array
-	 */
-	public $templates = [
-		'list'   => 'CodeIgniter\Validation\Views\list',
-		'single' => 'CodeIgniter\Validation\Views\single',
-	];
+  /**
+   * Specifies the views that are used to display the
+   * errors.
+   *
+   * @var array
+   */
+  public $templates = [
+    'list' => 'CodeIgniter\Validation\Views\list',
+    'single' => 'CodeIgniter\Validation\Views\single',
     'string' => 'App\Views\Validation\string'
+  ];
 
-	//--------------------------------------------------------------------
-	// Rules
-	//--------------------------------------------------------------------
+  //--------------------------------------------------------------------
+  // Rules
+  //--------------------------------------------------------------------
 
-	public $signup = [
-		'firstname' => 'required|name',
-		'lastname' => 'required|name',
-		'username' => 'required|username|max_length[20]|is_unique[user.username,id,{id}]',
-		'password' => 'required|min_length[8]',
-		'email' => 'required|valid_email|is_unique[user.email,id,{id}]'
-	];
+  public $signup = [
+    'firstname' => 'required|name',
+    'lastname' => 'required|name',
+    'username' => 'required|username|max_length[20]|is_unique[user.username,id,{id}]',
+    'password' => 'required|min_length[8]',
+    'email' => 'required|valid_email|is_unique[user.email,id,{id}]'
+  ];
 
-	public $login = [
+  public $login = [
     'login_name' => 'required',
     'login_password' => 'required',
   ];
