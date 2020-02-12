f638064c7c3cb96d71f2ac7edce6cf66d1c54206
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2020-02-12 18:37:10 +0000

    mod: moved game model functions back from facade
    in preparation for qp bot users

--- app/Facades/GameFacade.php
+++ app/Facades/GameFacade.php
@@ -1,10 +1,23 @@
 <?php namespace App\Facades;
 
+use App\Models\GameModel;
+
+/**
+ * Class GameFacade
+ * @package App\Facades
+ *
+ * @method static startTime(bool $startGame = false)
+ * @method static score($score = 0, $increment = true)
+ * @method static level(bool $increment = false, bool $reset = false)
+ * @method static reset()
+ *
+ * @mixin GameModel
+ */
 abstract class GameFacade extends BaseFacade
 {
-  private static $startTime;
-  private static $score;
-  private static $level;
+  private static $gameModel;
+
+  //--------------------------------------------------------------------
 
   /**
    * Initialise class static properties for session
@@ -13,60 +26,14 @@ abstract class GameFacade extends BaseFacade
   {
     parent::__static();
 
-    self::$startTime = & $_SESSION['startTime'];
-    self::$score = & $_SESSION['score'];
-    self::$level = & $_SESSION['level'];
-
-    self::$startTime || self::$startTime = null;
-    self::$level || self::$level = 1;
-    self::$score || self::$score = 0;
+    self::$gameModel || self::$gameModel = &$_SESSION['gameModel'];
+    self::$gameModel || self::$gameModel = new GameModel();
   }
 
   //--------------------------------------------------------------------
 
-  /**
-   * @param bool $startGame
-   * @return void|int
-   */
-  public static function startTime(bool $startGame = false)
+  public static function __callStatic($method, $args)
   {
-    if ($startGame) self::$startTime = time();
-    else return self::$startTime;
-  }
-
-  //--------------------------------------------------------------------
-
-  /**
-   * @param int $score
-   * @param bool $increment
-   * @return void|int
-   */
-  public static function score($score = 0, $increment = true)
-  {
-    if ($score && $increment) self::$score += $score;
-    elseif (!$increment) self::$score = $score;
-    else return self::$score;
-  }
-
-  //--------------------------------------------------------------------
-
-  /**
-   * @param bool $increment
-   * @param bool $reset
-   * @return void|int
-   */
-  public static function level(bool $increment = false, bool $reset = false)
-  {
-    if ($increment) self::$level++;
-    elseif ($reset) self::$level = 1;
-    else return self::$level;
-  }
-
-  //--------------------------------------------------------------------
-
-  public static function reset()
-  {
-    self::score(0);
-    self::level(false, true);
+    return self::callMethod(self::$gameModel, $method, $args);
   }
 }
--- app/Models/GameModel.php
+++ app/Models/GameModel.php
@@ -2,4 +2,64 @@
 
 class GameModel extends BaseModel
 {
+  private $startTime;
+  private $score;
+  private $level;
+
+  public function __construct()
+  {
+    parent::__construct();
+
+    $this->startTime = null;
+    $this->level = 1;
+    $this->score = 0;
+  }
+
+  //--------------------------------------------------------------------
+
+  /**
+   * @param bool $startGame
+   * @return void|int
+   */
+  public function startTime(bool $startGame = false)
+  {
+    if ($startGame) $this->startTime = time();
+    else return $this->startTime;
+  }
+
+  //--------------------------------------------------------------------
+
+  /**
+   * @param int $score
+   * @param bool $increment
+   * @return void|int
+   */
+  public function score($score = 0, $increment = true)
+  {
+    if ($score && $increment) $this->score += $score;
+    elseif (!$increment) $this->score = $score;
+    else return $this->score;
+  }
+
+  //--------------------------------------------------------------------
+
+  /**
+   * @param bool $increment
+   * @param bool $reset
+   * @return void|int
+   */
+  public function level(bool $increment = false, bool $reset = false)
+  {
+    if ($increment) $this->level++;
+    elseif ($reset) $this->level = 1;
+    else return $this->level;
+  }
+
+  //--------------------------------------------------------------------
+
+  public function reset()
+  {
+    $this->score(0);
+    $this->level(false, true);
+  }
 }
