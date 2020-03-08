00b479b5798244e3a39bc7ad417e72093af17692
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2020-03-08 00:22:07 +0000

    refactored facades
    use base facade to create new model object for each facade subclass

--- app/Facades/BaseFacade.php
+++ app/Facades/BaseFacade.php
@@ -1,6 +1,7 @@
 <?php namespace App\Facades;
 
 use App\Models\BaseModel;
+use CodeIgniter\Model;
 
 /**
  * Class BaseFacade
@@ -26,16 +27,27 @@ abstract class BaseFacade
 
   //--------------------------------------------------------------------
 
-  public static function __static()
+  public static function __callStatic(string $method, array $args)
   {
-    // 'testing' environment already init's session()
+    $className = substr(strrchr(static::class, "\\"), 1);
+    $className = str_replace('Facade', 'Model', $className);
+    $modelName = "\App\Models\\{$className}";
+
     // TODO: remove ENVIRONMENT check once CI4 session testing is possible
     ENVIRONMENT === 'testing' || session();
+
+    isset(self::$models[$modelName])
+     || self::$models[$modelName] = & $_SESSION[$modelName];
+
+    isset(self::$models[$modelName])
+     || self::$models[$modelName] = new $modelName();
+
+    return self::callMethod(self::$models[$modelName], $method, $args);
   }
 
   //--------------------------------------------------------------------
 
-  protected static function callMethod($model, $method, $args)
+  protected static function callMethod(Model $model, string $method, array $args)
   {
     switch(count($args))
     {
@@ -46,21 +58,5 @@ abstract class BaseFacade
       case 4: return $model->$method($args[0], $args[1], $args[2], $args[3]);
       default: return call_user_func_array([$model, $method], $args);
     }
-  }
-
-  //--------------------------------------------------------------------
-
-  public static function __callStatic($method, $args)
-  {
-    $className = substr(strrchr(static::class, "\\"), 1);
-    $className = str_replace('Facade', 'Model', $className);
-    $modelName = "\App\Models\\{$className}";
-
-    isset(self::$models[$modelName])
-     || self::$models[$modelName] = new $modelName();
-
-    $model = self::$models[$modelName];
-
-    self::callMethod($model, $method, $args);
   }
 }
--- app/Facades/GameFacade.php
+++ app/Facades/GameFacade.php
@@ -6,35 +6,12 @@ use App\Models\GameModel;
  * Class GameFacade
  * @package App\Facades
  *
+ * @method static reset()
  * @method static startTime(bool $startGame = false)
  * @method static score($score = false)
  * @method static level(bool $increment = false, bool $reset = false)
- * @method static scoreIsValid(float $remainder, int $diff = 0, int $count = null, string $ignoreIndices = null)
- * @method static reset()
+ * @method static scoreIsValid(float $remainder, int $sum = 0, int $count = null, string $ignoredIndices = null)
  *
  * @mixin GameModel
  */
-abstract class GameFacade extends BaseFacade
-{
-  private static $gameModel;
-
-  //--------------------------------------------------------------------
-
-  /**
-   * Initialise class static properties for session
-   */
-  public static function __static()
-  {
-    parent::__static();
-
-    self::$gameModel || self::$gameModel = &$_SESSION['gameModel'];
-    self::$gameModel || self::$gameModel = new GameModel();
-  }
-
-  //--------------------------------------------------------------------
-
-  public static function __callStatic($method, $args)
-  {
-    return self::callMethod(self::$gameModel, $method, $args);
-  }
-}
+abstract class GameFacade extends BaseFacade {}
